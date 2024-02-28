a7e40c6c478bf2083be93fb7785820659dde97de
MateoBrylando <161622218+MateoBrylando@users.noreply.github.com>
2024-02-28 16:26:21 +0000

    zmiana 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,10 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("cześć grzesiek kolego");
     System.out.println("Nara Grzesiek");
+    int c = 10;
+    int b = 12;
+    int sumka = c + b;
+    system.out.println("suma:" + sumka);
   }
 
   // @Test
